7d8af46af01d792e92ea9e675dce80d6c3c26e6e
Aleix Penella <aleix.penella@gmail.com>
2026-05-20 08:03:31 +0200

    update build-git-context-auth output test for not being order sensitive

--- examples/build-git-context-auth/build-git-context-auth_test.go
+++ examples/build-git-context-auth/build-git-context-auth_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
 
 	helper "github.com/apenella/go-docker-builder/internal/helpers"
@@ -25,6 +26,9 @@ custom: digest
 `
 
 	actual := helper.SanitizeDockerOutputForIntegrationTest(&buff)
+
+	expectedLines := strings.Split(strings.TrimSpace(expected), "\n")
+	assert.ElementsMatch(t, strings.Split(strings.TrimSpace(actual), "\n"), expectedLines)
 
 	assert.Equal(t, expected, actual)
 }
